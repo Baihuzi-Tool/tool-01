1e41d587e8c089910ad2a57fbb856d3690a4a6af
baihuzi <baihuzi8@gmail.com>
2020-04-07 20:39:51 +0800

    Initial commit

--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,10 @@
 <?php
 require_once __DIR__ . "/vendor/autoload.php";
 
+use Tool01\Route;
+
 if (!defined('PROJECT_DIR')) {
     define('PROJECT_DIR', __DIR__);
 }
 
-\Tool01\Route::init();
+Route::init();
